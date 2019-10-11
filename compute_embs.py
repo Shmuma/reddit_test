fe30c4312421c52f3ebdecb34a18453dea7df5a9
Max Lapan <max.lapan@gmail.com>
2019-10-11 19:39:07 +0300

    Cleanups

--- compute_embs.py
+++ compute_embs.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python3
 import argparse
-import csv
-import sys
-import tqdm
-import json
 import logging
-import collections
 import pathlib
 from fse import IndexedLineDocument, SplitIndexedList
 from fse.models import SIF
@@ -20,14 +15,13 @@ EXPECTED_LINES = 66836199
 
 if __name__ == "__main__":
     utils.setup_logging()
-    glove = api.load("glove-wiki-gigaword-100")
-
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--data", default=data.DEFAULT_OUTPUT_PREFIX,
                         help="Prefix of input data to read, default=" + data.DEFAULT_OUTPUT_PREFIX)
     parser.add_argument("--limit", type=int, help="Optional limit of input lines to process, default=No limit")
     args = parser.parse_args()
 
+    glove = api.load("glove-wiki-gigaword-100")
     input_path = pathlib.Path(args.data).with_suffix(".txt")
     sents = IndexedLineDocument(str(input_path))
     s = SplitIndexedList(sents)
